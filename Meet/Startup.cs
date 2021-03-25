f75856a866aea3b984651186cc29e0003db3ff0c
Abhishek Agrawal <abagra@microsoft.com>
2021-03-25 17:27:45 +0530

    checking for errors.

--- Meet/Startup.cs
+++ Meet/Startup.cs
@@ -31,7 +31,7 @@ namespace Meet
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -40,7 +40,8 @@ namespace Meet
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }*/
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
